1f7fea15f685221bb864b2e148b9835bedbcb635
Ayush <rishischnabel@gmail.com>
2026-04-25 19:42:07 +0530

    fix : new url

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5500",
   "http://127.0.0.1:5500",
- "https://ciphervest-frontend.vercel.app",
+  "https://ciphervest-frontend.vercel.app",
   "https://ciphervest.vercel.app",
   "https://www.ciphervestcapital.in",
   "https://ciphervestcapital.in",

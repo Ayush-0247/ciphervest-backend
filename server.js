5ff7d23ecd38a9cee2deab7d158a6e4b30f93464
Ayush <rishischnabel@gmail.com>
2026-04-25 19:36:11 +0530

    new url added

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ const app = express();
 
 
 
-
+https://ciphervest-frontend.vercel.app/
 
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5500",
   "http://127.0.0.1:5500",
+ "https://ciphervest-frontend.vercel.app",
   "https://ciphervest.vercel.app",
   "https://www.ciphervestcapital.in",
   "https://ciphervestcapital.in",
